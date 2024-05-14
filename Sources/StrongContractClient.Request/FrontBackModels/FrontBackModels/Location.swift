5d8c92c8d49eaf74b3ca7edc5b604361db7e305c
Leonardo Savio Dabus <leodabus@gmail.com>
2024-05-14 00:11:59 -0300

    Make location initializer public

--- Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Location.swift
+++ Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Location.swift
@@ -10,6 +10,10 @@ import Foundation
 public struct Location: Codable {
     public let latitude: Double
     public let longitude: Double
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
 
 public enum NotificationAuthorizationStatus: Int, Codable {
