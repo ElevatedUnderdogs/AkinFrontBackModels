53445c9659a68a9b48cd32cac0454468d368a3a0
scott-lydon <scottlydon18@gmail.com>
2024-06-21 22:02:50 -0400

    Add initiailzizer to the greet.settings

--- Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Greet+Components/MidGreetSettings.swift
+++ Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/Greet+Components/MidGreetSettings.swift
@@ -9,27 +9,37 @@
 import Foundation
 
 extension Greet {
-    
+
     public struct Settings: Codable {
         public var rejectedTimeProposals: [Int] = []
         public var agreedTimeProposals: [Int] = []
         public var status: Greet.Update.Status
 
+        public init(
+            rejectedTimeProposals: [Int] = [],
+            agreedTimeProposals: [Int] = [],
+            status: Greet.Update.Status
+        ) {
+            self.rejectedTimeProposals = rejectedTimeProposals
+            self.agreedTimeProposals = agreedTimeProposals
+            self.status = status
+        }
+
         public mutating func updateSettings(with otherUserSettings: Settings?) {
             guard let otherUserSettings = otherUserSettings else { return }
-            
+
             let commonProposedTimes = agreedTimeProposals.filter {
                 otherUserSettings.agreedTimeProposals.contains($0)
             }
             let validProposedTimes = commonProposedTimes.filter {
                 !rejectedTimeProposals.contains($0) && !otherUserSettings.rejectedTimeProposals.contains($0)
             }
-            
+
             if !validProposedTimes.isEmpty {
                 status = .enroute
             }
         }
-        
+
         public init(status: Greet.Update.Status) {
             self.status = status
         }
