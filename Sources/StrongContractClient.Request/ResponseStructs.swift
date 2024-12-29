f74cf80920af3f31893e0977ef72ff8f4b3551c9
Scott Lydon <scottlydon18@gmail.com>
2024-12-29 03:29:07 -0800

    Make the refresh token public.

--- Sources/StrongContractClient.Request/ResponseStructs.swift
+++ Sources/StrongContractClient.Request/ResponseStructs.swift
@@ -246,7 +246,7 @@ public struct LoginResponse: Codable {
 /// Helper struct for decoding the refresh token from the body
 /// Used to request a new access token
 public struct RefreshTokenRequestPayload: Codable {
-    let refreshToken: String
+    public let refreshToken: String
 
     public init(refreshToken: String) {
         self.refreshToken = refreshToken
