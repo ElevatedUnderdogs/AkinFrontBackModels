58ffcd0c981e0c096952938eafe6a3a4766b2cc7
Leonardo Savio Dabus <leodabus@gmail.com>
2025-01-12 05:04:28 -0300

    Make UserImage properties public

--- Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/UserImage.swift
+++ Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/UserImage.swift
@@ -9,8 +9,8 @@ import Foundation
 
 /// Provide UserImage with User ID and Image Name.
 public struct UserImage: Codable, Equatable {
-    let id: UUID
-    let name: String
+    public let id: UUID
+    public let name: String
     public init(id: UUID, name: String) {
         self.id = id
         self.name = name
