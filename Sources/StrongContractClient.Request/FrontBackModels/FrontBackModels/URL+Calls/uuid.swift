5c64eb43fe96a9c30df51500dc866171f2dbfda1
Scott Lydon <scottlydon18@gmail.com>
2025-03-10 02:17:30 -0700

    Make AddedAResponse struct and make the uuid for apnsid available as well.

---
+++ Sources/StrongContractClient.Request/FrontBackModels/FrontBackModels/URL+Calls/uuid.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  AkinFrontBackModels
+//
+//  Created by Scott Lydon on 3/9/25.
+//
+
+import Foundation
+
+extension UUID {
+
+    public static var questionResponseUpdated: UUID {
+        UUID(uuidString: "3F2504E0-4F89-41D3-9A0C-0305E82C3301")!
+    }
+}
