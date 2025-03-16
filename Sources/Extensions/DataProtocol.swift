ca95cabc0fb4211edc7f6467509f396df6404425
Scott Lydon <scottlydon18@gmail.com>
2025-03-15 17:43:26 -0700

    Update AkinFrontBackModels to StrongContractClient which removes vapor.  Update for vapor ommission.

--- Sources/Extensions/DataProtocol.swift
+++ Sources/Extensions/DataProtocol.swift
@@ -9,15 +9,30 @@ import protocol Foundation.DataProtocol
 
 #if canImport(Vapor)
 import Vapor
-#elseif canImport(Crypto)
-import Crypto
-#elseif canImport(CryptoKit)
-import CryptoKit
-#endif
 
-@available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
 extension DataProtocol {
     var sha256digest: SHA256Digest { SHA256.hash(data: self) }
     var sha384digest: SHA384Digest { SHA384.hash(data: self) }
     var sha512digest: SHA512Digest { SHA512.hash(data: self) }
 }
+#endif
+
+#if canImport(Crypto)
+import Crypto
+
+extension DataProtocol {
+    var sha256digest: SHA256Digest { SHA256.hash(data: self) }
+    var sha384digest: SHA384Digest { SHA384.hash(data: self) }
+    var sha512digest: SHA512Digest { SHA512.hash(data: self) }
+}
+#endif
+
+#if canImport(CryptoKit)
+import CryptoKit
+
+extension DataProtocol {
+    var sha256digest: SHA256Digest { SHA256.hash(data: self) }
+    var sha384digest: SHA384Digest { SHA384.hash(data: self) }
+    var sha512digest: SHA512Digest { SHA512.hash(data: self) }
+}
+#endif
