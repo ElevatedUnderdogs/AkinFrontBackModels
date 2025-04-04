df96076deac4e63166d11aae83bbbc684196d5a1
Scott Lydon <scottlydon18@gmail.com>
2025-04-03 23:55:32 -0700

    Add back the environment after revert.

---
+++ Sources/FrontBackModels/ServerEnvironment.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  AkinFrontBackModels
+//
+//  Created by Scott Lydon on 4/3/25.
+//
+
+import Foundation
+
+public enum ServerEnvironment: String, CaseIterable, Identifiable, Codable, Hashable, Equatable {
+    case dev, debug, prod
+    public var id: String { rawValue }
+}
